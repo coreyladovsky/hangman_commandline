57126840fbecb6312ec1d2eb2eb5f8f059c7ea4b
Corey Ladovsky <coreyskymusic@gmail.com>
2019-09-17 16:54:45 -0400

    game, humanPlayer

--- HumanPlayer.js
+++ HumanPlayer.js
@@ -2,7 +2,7 @@ const readlineSync = require('readline-sync');
 
 class HumanPlayer {
     getMove() {
-        return readlineSync.question("Please Guess A Letter: ");
+        return readlineSync.question("Please Guess A Letter: ").trim();
     }
 }
 
--- game.js
+++ game.js
@@ -6,12 +6,33 @@ class Game {
         this.board = new Board();
         this.humanPlayer = new HumanPlayer();
         this.guessesRemaining = 6;
+        this.alreadyGuessed = new Set(); 
     }
 
     play() {
         while(!this.isGameOver()) {
-            
+            this.displayBoard();
+            let guess = this.humanPlayer.getMove();
+            if(this.isValidGuess(guess)) {
+                this.addGuess(guess);
+                this.board.addChar(guess);
+                this.guessesRemaining--
+            }
         }
+
+        if(this.guessesRemaining) {
+            console.log("Congratulations! You Win! ")
+        } else {
+            console.log("You Lose, better luck next time")
+        }
+    }
+
+    isValidGuess(char) {
+        return char.length === 1 && char.match(/[a-z]/i) && !this.alreadyGuessed.has(char)
+    }
+
+    addGuess(char) {
+        this.alreadyGuessed.add(char);
     }
 
     isGameOver() {
